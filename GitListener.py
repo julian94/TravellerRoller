af2a4255dbbcccb6638140b96ee7d229cf0cdd00
Julian Silden Langlo <langlo94@gmail.com>
2021-02-23 23:12:25 +0100

    Listener now works properly.

--- GitListener.py
+++ GitListener.py
@@ -2,26 +2,46 @@ import git
 from flask import Flask, request
 import json
 import os
+import asyncio
 import sys
-import TravellerRoller
+import subprocess
 
 app = Flask(__name__)
+subprocess_handle = None
+subprocess_file = "/TravellerRoller.py"
 
 def git_pull():
 	g = git.cmd.Git(".")
 	g.pull()
 
+def start_subprocess():
+	global subprocess_handle
+	subprocess_handle = subprocess.Popen(
+		[sys.executable, subprocess_file],
+		stdout=subprocess.PIPE,
+		stderr=subprocess.STDOUT)
+	print(f"Started: {subprocess_file}")
+
+def stop_subprocess():
+	subprocess_handle.kill()
+	print(f"Terminated: {subprocess_file}")
+
 def update():
+	print("Pulling and restarting.")
 	git_pull()
-	os.execl(sys.executable, f"{sys.executable}", *sys.args)
+	stop_subprocess()
+	start_subprocess()
+	#os.execl(sys.executable, f"{sys.executable}", *sys.args)
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
+	print("Webhook Triggered.")
 	data = json.loads(request.data)
 	if data["action"] == "closed" and data["pull_request"]["merged"] == True:
 		update()
 	return "OK"
 
 if __name__ == '__main__':
+	start_subprocess()
+	stop_subprocess()
 	app.run(host="0.0.0.0", port=8087)
-	TravellerRoller.start()
